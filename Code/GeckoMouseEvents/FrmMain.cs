c6735e8227e2ce0bac2113c9333e7c39b6d815e4
dredei <xkadidas@gmail.com>
2015-04-28 00:04:48 +0300

    fix

--- Code/GeckoMouseEvents/FrmMain.cs
+++ Code/GeckoMouseEvents/FrmMain.cs
@@ -8,7 +8,7 @@ using Gecko;
 
 #region Copyright
 
-// Пример к статье: http://www.softez.pp.ua/2015/04/27/%d1%8d%d0%bc%d1%83%d0%bb%d1%8f%d1%86%d0%b8%d1%8f-%d1%81%d0%be%d0%b1%d1%8b%d1%82%d0%b8%d0%b9-%d0%bc%d1%8b%d1%88%d0%b8-%d0%b2-geckofx/
+// Пример к статье: http://www.softez.pp.ua/2015/04/27/mouse-events-emulation-geckofx/
 // Автор: dredei
 
 #endregion
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ GeckoMouseEvents
 
 Эмуляция событий мыши в GeckoFx (C#).
 
-Пример для статьи: http://www.softez.pp.ua/2015/04/27/%d1%8d%d0%bc%d1%83%d0%bb%d1%8f%d1%86%d0%b8%d1%8f-%d1%81%d0%be%d0%b1%d1%8b%d1%82%d0%b8%d0%b9-%d0%bc%d1%8b%d1%88%d0%b8-%d0%b2-geckofx/
+Пример для статьи: http://www.softez.pp.ua/2015/04/27/mouse-events-emulation-geckofx/
